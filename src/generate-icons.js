30181d69425e5f86c0d5e12c5a0eb85363226065
Neal Schultz <nschul4@github>
2026-08-26 20:47:48 +0100

    feat: update icon paths

--- src/generate-icons.js
+++ src/generate-icons.js
@@ -1,15 +1,15 @@
 import sharp from "sharp";
 
-const SVG_PATH = "./icon.svg";
+const SVG_PATH = "./images/icons/icon.svg";
 
 sharp(SVG_PATH)
   .resize(192, 192)
-  .toFile("./icon-192.png")
+  .toFile("./images/icons/icon-192.png")
   .then(() => console.log("✅ Generated icon-192.png"))
   .catch((err) => console.error(err));
 
 sharp(SVG_PATH)
   .resize(512, 512)
-  .toFile("./icon-512.png")
+  .toFile("./images/icons/icon-512.png")
   .then(() => console.log("✅ Generated icon-512.png"))
   .catch((err) => console.error(err));
